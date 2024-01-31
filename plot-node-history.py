86ddf84230bc35bc4534a4c309bb9e5d4b4a2476
Matthew Young <myoung.space.science@gmail.com>
2024-01-31 11:23:54 -0500

    Significantly modify plot-node-history.py (WIP)
    * Add CLI
    * Remove plot_flux, plot_divV, plot_radius, and plot_rtp
    * Define compute_history (directly from stream observer)
    * Remove get_array and parse_node_history: no longer necessary to use precomputed histories
    * Update/refactor main and plotting functions

--- plot-node-history.py
+++ plot-node-history.py
@@ -1,5 +1,4 @@
 import argparse
-import pathlib
 import typing
 import sys
 
@@ -11,206 +10,225 @@ import numpy
 import numpy.typing
 from scipy import signal
 
+from eprempy import atomic
 from eprempy import eprem
 from eprempy import paths
 from eprempy import physical
-from eprempy import atomic
 from eprempy import universal
 from tools.graphics import parse_plot_kws
 
-ERG_MEV = 1e6 * universal.CGS['eV']
+ERG_MEV = 1e6 * universal.CGS['eV'].asscalar
 R_SUN = 6.96e10
 
 
 def main(
     quantities: typing.List[str],
-    filedir: str=None,
-    pattern: str=None,
-    species: str='H+',
-    energy: float=0.0,
-    plot_kws: str=None,
-    verbose: bool=False,
+    n: int,
+    source: str,
+    step: int,
+    shell: int,
+    species: str,
+    energy: float,
+    kwargs_str: str,
+    outdir: str,
+    verbose: bool,
 ) -> None:
     """Plot node histories."""
-    readpath = paths.fullpath(filedir)
-    kwargs = parse_plot_kws(plot_kws or '')
+    kwargs = parse_plot_kws(kwargs_str or '')
+    stream = eprem.stream(n, source=source)
+    mosaic = []
     for quantity in quantities:
-        if quantity == 'dqdt':
+        if quantity.lower() == 'dqdt':
+            mosaic.extend([[quantity]]*3)
+        else:
+            mosaic.append([quantity])
+    fig, axd = plt.subplot_mosaic(
+        mosaic,
+        sharex=True,
+        figsize=(10, 10),
+    )
+    plt.xlabel('Time', fontsize=16)
+    for quantity in quantities:
+        ax = axd[quantity]
+        if quantity.lower() == 'dqdt':
             plot_dqdt_terms(
-                filedir=filedir,
-                pattern=pattern,
+                ax=ax,
+                stream=stream,
+                step=step,
+                shell=shell,
                 species=species,
                 energy=energy,
             )
         else:
-            filepath = readpath / f"{quantity}{pattern}.txt"
             plot_quantity_history(
-                filepath=filepath,
+                ax=ax,
+                quantity=quantity,
+                stream=stream,
+                step=step,
+                shell=shell,
+                species=species,
+                energy=energy,
                 **kwargs
             )
-    savename = f'{pattern}-{species}_{energy}MeV{pattern}.png'
-    savepath = readpath / savename
+    savename = f'history-{step}_{shell}_{species}_{energy}MeV.png'
+    savedir = paths.fullpath(outdir or '.')
+    savedir.mkdir(exist_ok=True, parents=True)
+    savepath = savedir / savename
     if verbose:
         print(f"Saving {savepath}")
     plt.savefig(savepath)
     plt.close()
 
 
+PLOT_KWS = {
+    'rho': {'yscale': 'log'},
+    'flux': {'yscale': 'log'},
+    'acceleration rate': {'yscale': 'log'},
+}
+
 def plot_quantity_history(
-    filepath: pathlib.Path,
-    **kwargs
+    ax: Axes,
+    quantity: str,
+    stream: eprem.Stream,
+    step: int,
+    shell: int,
+    species: str,
+    energy: float,
 ) -> None:
     """Plot the node history of one or more observable quantities."""
-    history = parse_node_history(filepath)
-    info = history['info'].copy()
-    plt.plot(history['time'], history['data'])
-    plt.xscale(kwargs.get('xscale', 'linear'))
-    plt.yscale(kwargs.get('yscale', 'linear'))
-    plt.xlabel(f"Time [{info['time unit']}]")
-    plt.ylabel(f"{info['data name']} [{info['data unit']}]")
+    time = numpy.array(stream.times)
+    array = compute_history(
+        quantity=quantity,
+        stream=stream,
+        step=step,
+        shell=shell,
+        species=species,
+        energy=energy,
+    )
+    ax.plot(time, array, color='black', label=quantity)
+    ax.set_yscale(PLOT_KWS.get('yscale', 'linear'))
+    # ax.legend(loc='lower right')
+    ax.grid(which='major', axis='both', linewidth=2)
+    ax.grid(which='minor', axis='both', linewidth=1)
+    # ax.yaxis.set_minor_locator(tck.MultipleLocator(1))
+    ax.set_ylabel(f"{quantity}")
 
 
 def plot_dqdt_terms(
-    readpath: pathlib.Path,
-    pattern: str=None,
-    species: str='H+',
-    energy: float=0.0,
+    ax: Axes,
+    stream: eprem.Stream,
+    step: int,
+    shell: int,
+    species: str,
+    energy: float,
 ) -> None:
     """Read and plot acceleration terms at one node as functions of time."""
+    # rho = compute_history('rho', stream, step, shell, species, energy)
+    # br = compute_history('br', stream, step, shell, species, energy)
+    # btheta = compute_history('btheta', stream, step, shell, species, energy)
+    # bphi = compute_history('bphi', stream, step, shell, species, energy)
+    # ur = compute_history('ur', stream, step, shell, species, energy)
+    # utheta = compute_history('utheta', stream, step, shell, species, energy)
+    # uphi = compute_history('uphi', stream, step, shell, species, energy)
     names = [
         'rho',
-        'bmag',
         'br', 'btheta', 'bphi',
-        'vr', 'vtheta', 'vphi',
+        'ur', 'utheta', 'uphi',
     ]
     arrays = {
-        name: get_array(readpath / f"{name}{pattern}.txt")
+        name: compute_history(name, stream, step, shell, species, energy)
         for name in names
     }
-    parsed = parse_node_history(readpath / f"{names[0]}{pattern}.txt")
-    time = 86400.0 * parsed['time']
-    # TODO: Can we get species and energy from the header, thereby precluding
-    # the need to pass them as arguments?
     s = atomic.species(species)
     e = physical.scalar(energy, unit='MeV')
-    vpart = numpy.sqrt(2 * e.withunit('erg') / s.mass.withunit('g')) # -> cm/s
-    groups = [
-        {'filter': False, 'linestyle': 'dotted'},
-        {'filter': True, 'linestyle': 'solid'},
-    ]
-    plt.figure(figsize=(10, 10))
-    mosaic = [
-        ['dQdt'],
-        ['dQdt'],
-        ['dQdt'],
-        ['flux'],
-        ['divV'],
-        ['radius'],
-    ]
-    fig, axd = plt.subplot_mosaic(
-        mosaic,
-        sharex=True,
-        figsize=(10, 10),
-    )
-    plt.sca(axd['dQdt'])
-    time_scale = 24.0 / 86400.0
+    v = numpy.sqrt(2 * e.withunit('erg') / s.mass.withunit('g')) # -> cm/s
+    time = numpy.array(stream['time'])
     plot_accel_terms(
-        axd['dQdt'],
-        time,
-        vpart=vpart,
+        ax=ax,
+        time=time,
+        v=v,
+        filter=False,
         **arrays,
-        time_scale=time_scale,
-        groups=groups,
+        linestyle='dotted',
     )
-    plt.sca(axd['flux'])
-    flux = get_array(readpath / f"flux{pattern}.txt")
-    plot_flux(axd['flux'], time_scale * time, flux, scale=1.0)
-    plt.sca(axd['divV'])
-    plot_divV(axd['divV'], time, arrays['rho'], time_scale=time_scale)
-    plt.sca(axd['radius'])
-    radius = get_array(readpath / f"radius{pattern}.txt")
-    plot_radius(axd['radius'], time_scale * time, radius=radius)
-    plt.xlabel('Time [hours]', fontsize=16)
+    plot_accel_terms(
+        ax=ax,
+        time=time,
+        v=v,
+        filter=True,
+        **arrays,
+        linestyle='solid',
+    )
+    ax.set_ylabel(r'$dQ/dt$ [$s^{-1}$]', fontsize=16)
+    ax.legend(title='Q', loc='upper right', ncol=2)
+    ax.set_ylim([-2e-3, +2e-3])
+    ax.grid(which='major', axis='both', linewidth=2)
+    ax.grid(which='minor', axis='both', linewidth=1)
+    # ax.xaxis.set_minor_locator(tck.MultipleLocator(0.25))
+    # ax.yaxis.set_minor_locator(tck.MultipleLocator(5))
+    ax.ticklabel_format(scilimits=(0, 0))
 
 
-def get_array(path: pathlib.Path) -> numpy.typing.NDArray:
-    """Get the data array from a node history."""
-    history = parse_node_history(path)
-    return history['data']
-
-
-def parse_node_history(
-    path: typing.Union[str, pathlib.Path]
-) -> typing.Dict[str, typing.Union[numpy.typing.NDArray, dict]]:
-    """Parse a text file containing a node history."""
-    filepath = paths.fullpath(path)
-    with filepath.open('r') as fp:
-        header = fp.readline()
-        time, data = numpy.loadtxt(fp, unpack=True)
-    info = header.lstrip('#').rstrip('\n').split(';')
-    result = {'time': time, 'data': data, 'info': {}}
-    for entry in info:
-        if entry:
-            k, v = entry.split(':')
-            result['info'][k.strip()] = v.strip()
-    return result
+# TODO: Consider passing in `times` and `observable` instead of `quantity` and
+# `stream`.
+def compute_history(
+    quantity: str,
+    stream: eprem.Stream,
+    step: int,
+    shell: int,
+    species: str,
+    energy: float,
+) -> numpy.typing.NDArray:
+    """Compute the history of a named quantity on a certain node."""
+    observable = stream[quantity]
+    indices = [slice(None), slice(None)]
+    if 'species' in observable.dimensions:
+        indices.append(species)
+    if 'energy' in observable.dimensions:
+        indices.append((energy, 'MeV'))
+    array = observable[*tuple(indices)]
+    times = stream.times.withunit('day')
+    ntimes = len(times)
+    ts = zip(range(step, step+ntimes), range(shell, shell+ntimes))
+    return numpy.squeeze([array[t-step, s-step, ...] for t, s in ts])
 
 
 def plot_accel_terms(
     ax: Axes,
     time: numpy.typing.NDArray,
     rho: numpy.typing.NDArray,
-    bmag: numpy.typing.NDArray,
-    vr: numpy.typing.NDArray,
-    vtheta: numpy.typing.NDArray,
-    vphi: numpy.typing.NDArray,
+    ur: numpy.typing.NDArray,
+    utheta: numpy.typing.NDArray,
+    uphi: numpy.typing.NDArray,
     br: numpy.typing.NDArray,
     btheta: numpy.typing.NDArray,
     bphi: numpy.typing.NDArray,
-    vpart: numpy.typing.NDArray,
-    time_scale: float=1.0,
-    colors: typing.List[str]=None,
-    groups: typing.Iterable[dict]=None,
-    **plot_kw
+    v: float,
+    filter: bool,
+    **kwargs
 ) -> None:
     """Compute and plot acceleration terms as functions of time."""
-    groups = groups or {}
-    for group in groups:
-        filter_on = group.get('filter', False)
-        if filter_on:
-            rho = smooth(rho)
-            bmag = smooth(bmag)
-        rho_b = rho / bmag
-        dln_rho_dt = numpy.gradient(numpy.log(rho), time)
-        dln_rho_b_dt = numpy.gradient(numpy.log(rho_b), time)
-        dln_b_dt = numpy.gradient(numpy.log(bmag), time)
-        vb = (br*vr + btheta*vtheta + bphi*vphi) / bmag
-        dvb_dt = numpy.gradient(vb, time)
-
-        sm = ' [smoothed]' if filter_on else ''
-        quantities = {
-            rf'$\ln({{n/B}})${sm}': dln_rho_b_dt,
-            rf'$\ln({{B}})${sm}': dln_b_dt,
-            rf'$\ln({{n/B}}) + \ln({{B}})${sm}': dln_rho_b_dt + dln_b_dt,
-            # rf'$\ln({{n}})${sm}': dln_rho_dt,
-            rf'$-\hat{{b}}\cdot\vec{{V}}/w${sm}': -dvb_dt / vpart,
-        }
-        colors = colors or [f'C{i}' for i in range(len(quantities))]
-        if 'linestyle' in group:
-            plot_kw['linestyle'] = group['linestyle']
-
-        t = time * time_scale
-        for color, (label, physics) in zip(colors, quantities.items()):
-            plt.plot(t, physics, color=color, label=label, **plot_kw)
-
-    plt.ylabel(r'$dQ/dt$ [$s^{-1}$]', fontsize=16)
-    plt.legend(title='Q', loc='upper right', ncol=2)
-    plt.ylim([-2e-3, +2e-3])
-    plt.grid(which='major', axis='both', linewidth=2)
-    plt.grid(which='minor', axis='both', linewidth=1)
-    ax.xaxis.set_minor_locator(tck.MultipleLocator(0.25))
-    ax.yaxis.set_minor_locator(tck.MultipleLocator(5))
-    ax.ticklabel_format(scilimits=(0, 0))
+    bmag = numpy.sqrt(br**2 + btheta**2 + bphi**2)
+    if filter:
+        rho = smooth(rho)
+        bmag = smooth(bmag)
+    rho_b = rho / bmag
+    dln_rho_dt = numpy.gradient(numpy.log(rho), time)
+    dln_rho_b_dt = numpy.gradient(numpy.log(rho_b), time)
+    dln_b_dt = numpy.gradient(numpy.log(bmag), time)
+    ub = (br*ur + btheta*utheta + bphi*uphi) / bmag
+    dub_dt = numpy.gradient(ub, time)
+    smstr = ' [smoothed]' if filter else ''
+    quantities = {
+        rf'$\ln({{n/B}})${smstr}': dln_rho_b_dt,
+        rf'$\ln({{B}})${smstr}': dln_b_dt,
+        rf'$\ln({{n/B}}) + \ln({{B}})${smstr}': dln_rho_b_dt + dln_b_dt,
+        # rf'$\ln({{n}})${smstr}': dln_rho_dt,
+        rf'$-\hat{{b}}\cdot\vec{{V}}/w${smstr}': -dub_dt / v,
+    }
+    colors = [f'C{i}' for i in range(len(quantities))]
+    for color, (label, array) in zip(colors, quantities.items()):
+        ax.plot(time, array, color=color, label=label, **kwargs)
 
 
 def smooth(x) -> numpy.typing.NDArray:
@@ -219,87 +237,69 @@ def smooth(x) -> numpy.typing.NDArray:
     return numpy.where(xs > 0, xs, sys.float_info.min)
 
 
-def plot_flux(
-    ax: Axes,
-    time: numpy.typing.NDArray,
-    flux: numpy.typing.NDArray,
-    scale: float=1.0,
-) -> None:
-    """Plot flux at this node as a function of time."""
-    if scale != 1.0:
-        tmp_str = f"{scale:e}"
-        exp = int(tmp_str.split('e')[1])
-        scale_str = fr' $\times$ $10^{{{exp}}}$'
-    else:
-        scale_str = ''
-    label = fr'J(E){scale_str} [# / cm$^2$ s sr (MeV/nuc)]'
-    plt.plot(time, flux * ERG_MEV * scale, 'k.', label=label)
-    plt.legend(loc='upper right')
-    plt.grid(which='major', axis='both', linewidth=2)
-    plt.grid(which='minor', axis='both', linewidth=1)
-    ax.yaxis.set_minor_locator(tck.MultipleLocator(1))
-
-
-def plot_divV(
-    ax: Axes,
-    time: numpy.typing.NDArray,
-    rho: numpy.typing.NDArray,
-    time_scale: float=1.0,
-) -> None:
-    """Plot div(V) at this node as a function of time.
-    
-    Notes
-    -----
-    - div(V) = -dln(n)/dt from the convective (co-moving) continuity equation.
-    """
-    label = r"$-\frac{1}{3}\nabla\cdot\vec{V}$"
-    divV = -numpy.gradient(numpy.log(rho), time)
-    plt.plot(time_scale * time, -(1 / 3) * divV, 'g-', label=label)
-    plt.ylim([-2e-4, 0])
-    plt.legend(loc='lower right')
-    plt.grid(which='major', axis='both', linewidth=2)
-    plt.grid(which='minor', axis='both', linewidth=1)
-    ax.yaxis.set_minor_locator(tck.MultipleLocator(1))
-    ax.ticklabel_format(scilimits=(0, 0))
-
-
-def plot_radius(
-    ax: Axes,
-    time: numpy.typing.NDArray,
-    radius: numpy.typing.NDArray=None,
-) -> None:
-    """Plot the node radius as a function of time."""
-    label = r'r [$R_\odot$]'
-    plt.plot(time, radius / R_SUN, label=label)
-    plt.ylim([0, 5])
-    plt.legend(loc='lower right')
-    plt.grid(which='major', axis='both', linewidth=2)
-    plt.grid(which='minor', axis='both', linewidth=1)
-    ax.yaxis.set_minor_locator(tck.MultipleLocator(1))
-
-
-def plot_rtp(
-    time: numpy.typing.NDArray,
-    radius: numpy.typing.NDArray=None,
-    theta: numpy.typing.NDArray=None,
-    phi: numpy.typing.NDArray=None,
-) -> mlines.Line2D:
-    """Plot the node radius as a function of time."""
-    lines = []
-    if radius is not None:
-        label = r'r [$R_\odot$]'
-        lines.extend(plt.plot(time, radius / R_SUN, 'k--', label=label))
-    if theta is not None:
-        label = r'$\theta$'
-        lines.extend(plt.plot(time, theta, 'r--', label=label))
-    if phi is not None:
-        label = r'$\phi$'
-        lines.extend(plt.plot(time, phi, 'r', label=label))
-    return lines
-
-
-# CLI defaults:
-# - step = 0
-# - shell = 0
-# - species = 'H+'
-# - energy ['MeV'] = 0.0
+if __name__ == '__main__':
+    p = argparse.ArgumentParser(
+        description=main.__doc__,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    p.add_argument(
+        'quantities',
+        help="observable quantities to plot; may include 'dQdt'",
+        nargs='*',
+    )
+    p.add_argument(
+        '--stream',
+        dest='n',
+        help="the stream number to use",
+        type=int,
+        default=0,
+    )
+    p.add_argument(
+        '--timestep',
+        dest='step',
+        help="the time step (0-based) at which to identify the target node",
+        type=int,
+        default=0,
+    )
+    p.add_argument(
+        '--shell',
+        help="the shell (0-based) on which to identify the target node",
+        type=int,
+        default=0,
+    )
+    p.add_argument(
+        '--species',
+        help="the target atomic species, if applicable (default: H+)",
+        default='H+',
+    )
+    p.add_argument(
+        '--energy',
+        help="the target energy (in MeV), if applicable (default: 0.0)",
+        type=float,
+        default=0.0,
+    )
+    p.add_argument(
+        '--plot_kws',
+        dest='kwargs_str',
+        help=(
+            "key-value pairs to pass to plotting routines"
+            "\nPass multiple comma-separated key-value pairs"
+            " as a single quoted string"
+        ),
+    )
+    p.add_argument(
+        '-i', '--indir',
+        dest='source',
+        help="the directory containing the EPREM output (default: ./)",
+    )
+    p.add_argument(
+        '-o', '--outdir',
+        help="the directory in which to save the plot (default: ./)",
+    )
+    p.add_argument(
+        '-v', '--verbose',
+        help="print runtime messages",
+        action='store_true',
+    )
+    args = p.parse_args()
+    main(**vars(args))
