925c3d08f1beac2594da04b1c7025c93df14055c
Matthew Young <matthew.young@unh.edu>
2024-05-16 13:43:19 -0400

    Update stream-survey.py
    * Add y-axis limits to CLI
    * Set or compute y-axis limits in functions
    * Rename variable

--- stream-survey.py
+++ stream-survey.py
@@ -22,12 +22,10 @@ def main(
     source = indir or '.'
     dataset = eprem.dataset(source=source, config=config)
     streams = get_streams(dataset, num)
-    location = get_location(user)
-    species = get_species(user)
     plotdir = fullpath(outdir or source)
     plotdir.mkdir(parents=True, exist_ok=True)
     for stream in streams:
-        plot_stream(stream, location=location, species=species)
+        plot_stream(stream, **user)
         plotpath = plotdir / stream.source.with_suffix('.png').name
         if verbose:
             print(f"Saved {plotpath}")
@@ -35,7 +33,7 @@ def main(
         plt.close()
 
 
-def plot_stream(stream: eprem.Observer, **kwargs):
+def plot_stream(stream: eprem.Observer, **user):
     """Create a survey plot for this stream."""
     fig, axs = plt.subplots(
         nrows=1,
@@ -44,10 +42,15 @@ def plot_stream(stream: eprem.Observer, **kwargs):
         figsize=(20, 6),
         layout='constrained',
     )
-    plot_stream_flux(axs[0], stream, **kwargs)
-    plot_stream_fluence(axs[1], stream, **kwargs)
-    plot_stream_intflux(axs[2], stream, **kwargs)
-    fig.suptitle(make_suptitle(stream, **kwargs), fontsize=20)
+    location = get_location(user)
+    species = get_species(user)
+    ylim = user.get('flux_ylim')
+    plot_stream_flux(axs[0], stream, location, species, ylim)
+    ylim = user.get('fluence_ylim')
+    plot_stream_fluence(axs[1], stream, location, species, ylim)
+    ylim = user.get('intflux_ylim')
+    plot_stream_intflux(axs[2], stream, location, species, ylim)
+    fig.suptitle(make_suptitle(stream, location, species), fontsize=20)
 
 
 def plot_stream_flux(
@@ -55,6 +58,7 @@ def plot_stream_flux(
     stream: eprem.Stream,
     location: typing.Union[int, quantity.Measurement],
     species: typing.Union[int, str],
+    ylim: typing.Tuple[float, float],
 ) -> None:
     """Create a plot of flux versus time for this stream."""
     flux = stream['flux'].withunit('1 / (cm^2 s sr MeV/nuc)')
@@ -69,8 +73,7 @@ def plot_stream_flux(
             yvalmax = arraymax
         else:
             yvalmax = max(yvalmax, arraymax)
-    ylogmax = int(numpy.log10(yvalmax)) + 1
-    ax.set_ylim([10**(ylogmax-6), 10**ylogmax])
+    ax.set_ylim(ylim or compute_yloglim(yvalmax))
     ax.set_xlabel(f"Time [{stream.times.unit}]", fontsize=14)
     ax.set_ylabel(r"Flux [1 / (cm$^2$ s sr MeV/nuc)]", fontsize=14)
     ax.set_xscale('linear')
@@ -83,14 +86,14 @@ def plot_stream_fluence(
     stream: eprem.Stream,
     location: typing.Union[int, quantity.Measurement],
     species: typing.Union[int, str],
+    ylim: typing.Tuple[float, float],
 ) -> None:
     """Create a plot of fluence versus energy for this stream."""
     fluence = stream['fluence'].withunit('1 / (cm^2 sr MeV/nuc)')
     energies = stream.energies.withunit('MeV')
     array = fluence[-1, location, species, :].squeezed
     ax.plot(energies, array)
-    ylogmax = int(numpy.log10(numpy.max(array))) + 1
-    ax.set_ylim([10**(ylogmax-6), 10**ylogmax])
+    ax.set_ylim(ylim or compute_yloglim(numpy.max(array)))
     ax.set_xlabel(f"Energy [{energies.unit}]", fontsize=14)
     ax.set_ylabel(r"Fluence [1 / (cm$^2$ sr MeV/nuc)]", fontsize=14)
     ax.set_xscale('log')
@@ -102,6 +105,7 @@ def plot_stream_intflux(
     stream: eprem.Stream,
     location: typing.Union[int, quantity.Measurement],
     species: typing.Union[int, str],
+    ylim: typing.Tuple[float, float],
 ) -> None:
     """Create a plot of integral flux versus time for this stream."""
     intflux = stream['integral flux'].withunit('1 / (cm^2 s sr)')
@@ -116,13 +120,18 @@ def plot_stream_intflux(
             yvalmax = arraymax
         else:
             yvalmax = max(yvalmax, arraymax)
-    ylogmax = int(numpy.log10(yvalmax)) + 1
-    ax.set_ylim([10**(ylogmax-6), 10**ylogmax])
+    ax.set_ylim(ylim or compute_yloglim(yvalmax))
     ax.set_xlabel(f"Time [{stream.times.unit}]", fontsize=14)
     ax.set_ylabel(r"Integral Flux [1 / (cm$^2$ s sr)]", fontsize=14)
     ax.set_xscale('linear')
     ax.set_yscale('log')
     ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), handlelength=1.0)
+
+
+def compute_yloglim(maxval):
+    """Compute logarithmic y-axis limits based on `maxval`."""
+    ylogmax = int(numpy.log10(maxval)) + 1
+    return 10**(ylogmax-6), 10**ylogmax
 
 
 def get_streams(dataset: eprem.Dataset, num: typing.Optional[int]=None):
@@ -215,6 +224,27 @@ if __name__ == '__main__':
             "ion species to plot"
             "; may be symbol or index (default: 0)"
         ),
+    )
+    parser.add_argument(
+        '--flux-ylim',
+        help="y-axis limits for flux",
+        nargs=2,
+        type=float,
+        metavar=('LO', 'HI'),
+    )
+    parser.add_argument(
+        '--fluence-ylim',
+        help="y-axis limits for fluence",
+        nargs=2,
+        type=float,
+        metavar=('LO', 'HI'),
+    )
+    parser.add_argument(
+        '--intflux-ylim',
+        help="y-axis limits for integral flux",
+        nargs=2,
+        type=float,
+        metavar=('LO', 'HI'),
     )
     parser.add_argument(
         '-v', '--verbose',
