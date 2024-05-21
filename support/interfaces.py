4f6cb03306529d16131d3ef8250ee2af1fa81e9b
Matthew Young <myoung.space.science@gmail.com>
2024-05-21 16:46:00 -0400

    Refactor support modules
    * Move parse_plot_kws (currently not in use)
    to interfaces.py from graphics.py
    * Remove graphics.py

--- support/interfaces.py
+++ support/interfaces.py
@@ -68,3 +68,15 @@ def split_key_value_pairs(
     return target
 
 
+def parse_plot_kws(string: str) -> typing.Dict[str, typing.Any]:
+    """Parse plot-related key-value pairs from CLI."""
+    if not string:
+        return {}
+    pairs = string.split(',')
+    result = {}
+    for pair in pairs:
+        i = pair.split('=')
+        result[i[0].strip()] = i[1]
+    return result
+
+
